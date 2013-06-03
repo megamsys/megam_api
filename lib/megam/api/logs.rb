99902ad7d07bc5758dca29323340e52890f792b1
thomasalrin <alrin@megam.co.in>
2013-06-03 15:31:04 +0530

    Node and log skeleton refresh

--- lib/megam/api/logs.rb
+++ lib/megam/api/logs.rb
@@ -2,12 +2,15 @@ module Megam
   class API
 
     # GET /logs/:node_id
-    def get_logs(node_id, options = {})      
+    def get_logs(node_id)      
+       @options = {:path => '/logs/#{node_id}',
+       :body => OkJson.encode({"node_id" => "#{node_id}"})}.merge(@options)
+
       request(
         :expects  => 200,
         :method   => :get,
-        :path     => "/logs/#{node_id}",
-        :query    => options
+        :path     => @options[:path],
+        :body     => @options[:body]
       )
     end
 
--- lib/megam/api/nodes.rb
+++ lib/megam/api/nodes.rb
@@ -3,30 +3,45 @@ module Megam
 
     # GET /nodes
     def get_nodes
+      @options = {:path => '/nodes',
+        :body => OkJson.encode({"email" => "#{email}"})}.merge(@options)
+
       request(
         :expects  => 200,
         :method   => :get,
-        :path     => "/nodes"
+        :path     => @options[:path],
+        :body     => @options[:body]
       )
     end
 
     # GET /nodes/:id
     def get_node(node_id)
+      @options = {:path => '/nodes/#{node_id}',
+        :body => OkJson.encode({"node_id" => "#{node_id}"})}.merge(@options)
+
       request(
         :expects  => 200,
         :method   => :get,
-        :path     => "/nodes/#{node_id}"
+        :path     => @options[:path],
+        :body     => @options[:body]
       )
     end
 
     
     # POST /nodes/content
-    def post_node(params={})
+    def post_node(id, email, api_key, user_type)
+      @options = {:path => '/nodes/content', :body => OkJson.encode({
+        "id" => "#{id}",
+        "email" => email,
+        "api_key" => api_key,
+        "authority" => "#{user_type}"
+      })}.merge(@options)
+
       request(
-        :expects  => 202,
+        :expects  => 200,
         :method   => :post,
-        :path     => '/nodes/content',
-        :query    => nodes_params(params)
+        :path     => @options[:path],
+        :body     => @options[:body]
       )
     end
  
@@ -34,10 +49,14 @@ module Megam
     
     # DELETE /nodes/:node_id
     def delete_node(node_id)
+      @options = {:path => '/nodes/#{node_id}',
+        :body => OkJson.encode({"node_id" => "#{node_id}"})}.merge(@options)
+
       request(
         :expects  => 200,
         :method   => :delete,
-        :path     => "/nodes/#{node_id}"
+        :path     => @options[:path],
+        :body     => @options[:body]
       )
     end
 
