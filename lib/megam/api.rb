5565d156a147483c01ca21d8d25ef9f32022cf71
Kishorekumar Neelamegam <nkishore@megam.co.in>
2013-07-22 08:50:00 +0530

    changes

--- lib/megam/api.rb
+++ lib/megam/api.rb
@@ -6,8 +6,6 @@ require "uri"
 require "zlib"
 require 'openssl'
 
-#require "iconv"
-
 __LIB_DIR__ = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 unless $LOAD_PATH.include?(__LIB_DIR__)
 $LOAD_PATH.unshift(__LIB_DIR__)
