a952e0dd801369b9203bf91cbfebee123cfe86ff
Kishorekumar Neelamegam <nkishore@megam.co.in>
2013-12-04 20:54:49 +0530

    just a source > format.

--- lib/megam/builder/make_node.rb
+++ lib/megam/builder/make_node.rb
@@ -21,64 +21,63 @@ module Megam
     end
 
     def self.create(data, group, action, email)
-    
-       make_command = self.new()
-     begin
-      pc_collection = make_command.megam_rest.get_predefclouds
-      ct_collection = make_command.megam_rest.get_cloudtools
-      
-    rescue ArgumentError => ae
-      hash = {"msg" => ae.message, "msg_type" => "error"}
-      re = Megam::Error.from_hash(hash)
-      return re
-    rescue Megam::API::Errors::ErrorWithResponse => ewr
-      hash = {"msg" => ewr.message, "msg_type" => "error"}
-      re = Megam::Error.from_hash(hash)
-      return re
-    rescue StandardError => se
-      hash = {"msg" => se.message, "msg_type" => "error"}
-      re = Megam::Error.from_hash(hash)
-      return re
-    end
-    
-    
-    predef_cloud = pc_collection.data[:body].lookup("#{data[:predef_cloud_name]}")
-tool = ct_collection.data[:body].lookup(data[:provider])
-template = tool.cloudtemplates.lookup(predef_cloud.spec[:type_name])
-cloud_instruction = template.lookup_by_instruction(group, action)
-ci_command = cloud_instruction.command
-ci_name = cloud_instruction.name
 
-command_hash = {
-"systemprovider" => {
-"provider" => {
-"prov" => "#{data[:provider]}"
-}
-},
-"compute" => {
-"cctype" => "#{predef_cloud.spec[:type_name]}",
-"cc"=> {
-"groups" => "#{predef_cloud.spec[:groups]}",
-"image" => "#{predef_cloud.spec[:image]}",
-"flavor" => "#{predef_cloud.spec[:flavor]}"
-},
-"access" => {
-"ssh_key" => "#{predef_cloud.access[:ssh_key]}",
-"identity_file" => email+"/"+"#{data[:predef_cloud_name]}"+"/"+"#{predef_cloud.access[:identity_file]}",
-"ssh_user" => "#{predef_cloud.access[:ssh_user]}",
-"vault_location" => "#{predef_cloud.access[:vault_location]}",
-"sshpub_location" => "#{predef_cloud.access[:sshpub_location]}"
-}
-},
-"cloudtool" => {
-"chef" => {
-"command" => "#{tool.cli}",
-"plugin" => "#{template.cctype} #{ci_command}",
-"run_list" => "'role[#{data[:provider_role]}]'",
-"name" => "#{ci_name} #{data[:book_name]}"
-}
-}
-}
+      make_command = self.new()
+      begin
+        pc_collection = make_command.megam_rest.get_predefclouds
+        ct_collection = make_command.megam_rest.get_cloudtools
+
+      rescue ArgumentError => ae
+        hash = {"msg" => ae.message, "msg_type" => "error"}
+        re = Megam::Error.from_hash(hash)
+        return re
+      rescue Megam::API::Errors::ErrorWithResponse => ewr
+        hash = {"msg" => ewr.message, "msg_type" => "error"}
+        re = Megam::Error.from_hash(hash)
+        return re
+      rescue StandardError => se
+        hash = {"msg" => se.message, "msg_type" => "error"}
+        re = Megam::Error.from_hash(hash)
+      return re
+      end
+
+      predef_cloud = pc_collection.data[:body].lookup("#{data[:predef_cloud_name]}")
+      tool = ct_collection.data[:body].lookup(data[:provider])
+      template = tool.cloudtemplates.lookup(predef_cloud.spec[:type_name])
+      cloud_instruction = template.lookup_by_instruction(group, action)
+      ci_command = cloud_instruction.command
+      ci_name = cloud_instruction.name
+
+      command_hash = {
+        "systemprovider" => {
+          "provider" => {
+            "prov" => "#{data[:provider]}"
+          }
+        },
+        "compute" => {
+          "cctype" => "#{predef_cloud.spec[:type_name]}",
+          "cc"=> {
+            "groups" => "#{predef_cloud.spec[:groups]}",
+            "image" => "#{predef_cloud.spec[:image]}",
+            "flavor" => "#{predef_cloud.spec[:flavor]}"
+          },
+          "access" => {
+            "ssh_key" => "#{predef_cloud.access[:ssh_key]}",
+            "identity_file" => email+"/"+"#{data[:predef_cloud_name]}"+"/"+"#{predef_cloud.access[:identity_file]}",
+            "ssh_user" => "#{predef_cloud.access[:ssh_user]}",
+            "vault_location" => "#{predef_cloud.access[:vault_location]}",
+            "sshpub_location" => "#{predef_cloud.access[:sshpub_location]}"
+          }
+        },
+        "cloudtool" => {
+          "chef" => {
+            "command" => "#{tool.cli}",
+            "plugin" => "#{template.cctype} #{ci_command}",
+            "run_list" => "'role[#{data[:provider_role]}]'",
+            "name" => "#{ci_name} #{data[:book_name]}"
+          }
+        }
+      }
 
       node_hash = {
         "node_name" => "#{data[:book_name]}#{data[:domain_name]}",
@@ -101,7 +100,7 @@ command_hash = {
         node_hash["boltdefns"] = {"username" => "#{data['user_name']}", "apikey" => "#{data['password']}", "store_name" => "#{data['store_db_name']}", "url" => "#{data['url']}", "prime" => "#{data['prime']}", "timetokill" => "#{data['timetokill']}", "metered" => "#{data['monitoring']}", "logging" => "#{data['logging']}", "runtime_exec" => "#{data['runtime_exec']}" }
       end
 
-node_hash
+      node_hash
     end
   end
 end
