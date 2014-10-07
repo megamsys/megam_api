88bdf7b2d804791e9a9d550dbc1bee4b0488e0d3
morpheyesh <morpheyesh@gmail.com>
2014-10-07 19:20:14 +0530

    organizations done

---
+++ lib/megam/api/organizations.rb
@@ -0,0 +1,30 @@
+module Megam
+  class API
+
+def get_organizations(name)
+
+  @options = {:path => "/organizations/#{name}",
+    :body => ''}.merge(@options)
+
+  request(
+    :expects  => 200,
+    :method   => :get,
+    :body     => @options[:body]
+  )
+end
+
+
+def post_organizations(new_organization)
+
+      @option = {:path => '/organizations/content',
+        :body =>  Megam::JSONCompat.to_json(new_organization)}.merge(@options)
+
+
+              request(
+                :expects  => 201,
+                :method   => :post,
+                :body     => @options[:body]
+                )
+            end
+      end
+end
