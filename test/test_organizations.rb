3a653f95b2c19b9317bcc9b85bd4a731f0b154bf
morpheyesh <morpheyesh@gmail.com>
2014-10-24 02:58:15 +0530

    orgfix

--- test/test_organizations.rb
+++ test/test_organizations.rb
@@ -7,9 +7,15 @@ class TestOrganizations < MiniTest::Unit::TestCase
   $tom_email = "tom@gomegam.com"
   $bob_email = "bob@gomegam.com"
 
+ def test_get_organizations_good1
+    response =megams.get_organizations
+    response.body.to_s
+    assert_equal(200, response.status)
+ end
+ 
 
   def test_get_organizations_good
-    response =megams.get_organizations(sandbox_name)
+    response =megams.get_organization(sandbox_name)
     response.body.to_s
     assert_equal(200, response.status)
   end
@@ -18,7 +24,7 @@ class TestOrganizations < MiniTest::Unit::TestCase
   def test_post_organizations_good
     tmp_hash = {
      "name" => "orgname3"}
-    response =megams.post_organizations(tmp_hash)
+    response =megams.post_organization(tmp_hash)
     response.body.to_s
     assert_equal(201, response.status)
   end
