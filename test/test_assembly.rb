6b0fcfcc232be3656f38e9290e0c289f3f1e0489
rajthilakmca <rajthilak@megam.co.in>
2014-11-27 18:30:11 +0530

    Add status in assembly

--- test/test_assembly.rb
+++ test/test_assembly.rb
@@ -20,6 +20,7 @@ class TestApps < MiniTest::Unit::TestCase
       "inputs" => "",
       "operations" => "",
       "output" => [],
+      "status" => "Launching",
       "created_at" => "2014-10-29 13:24:06 +0000"
     }
 
