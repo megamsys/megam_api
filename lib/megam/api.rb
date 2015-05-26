8121c3b4e50216c47a8d0cd6b652c3d373836408
indykish <nkishore@megam.io>
2015-05-26 15:48:17 +0530

    minor updates to readme.md

--- lib/megam/api.rb
+++ lib/megam/api.rb
@@ -1,110 +1,105 @@
-require "base64"
-require "time"
-require "excon"
-require "uri"
-require "zlib"
+require 'base64'
+require 'time'
+require 'excon'
+require 'uri'
+require 'zlib'
 require 'openssl'
-require 'yaml'                  #COMMON YML
 # open it up when needed. This will be needed when a new customer onboarded via pug.
-require "securerandom"
+require 'securerandom'
 
-__LIB_DIR__ = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-unless $LOAD_PATH.include?(__LIB_DIR__)
-$LOAD_PATH.unshift(__LIB_DIR__)
-end
+__LIB_DIR__ = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+$LOAD_PATH.unshift(__LIB_DIR__) unless $LOAD_PATH.include?(__LIB_DIR__)
 
-require "megam/api/errors"
-require "megam/api/version"
-require "megam/api/accounts"
-require "megam/api/requests"
-require "megam/api/cat_requests"
-require "megam/api/predef_clouds"
-require "megam/api/cloud_tool_settings"
-require "megam/api/sshkeys"
-require "megam/api/marketplaces"
-require "megam/api/marketplace_addons"
-require "megam/api/organizations"
-require "megam/api/domains"
-require "megam/api/csars"
-require "megam/api/assemblies"
-require "megam/api/assembly"
-require "megam/api/components"
-require "megam/api/event"
-require "megam/api/availableunits"
-require "megam/api/balances"
-require "megam/api/billinghistories"
-require "megam/api/billings"
-require "megam/api/credithistories"
-require "megam/api/discounts"
-require "megam/api/subscriptions"
-require "megam/api/promos"
+require 'megam/api/errors'
+require 'megam/api/version'
+require 'megam/api/accounts'
+require 'megam/api/requests'
+require 'megam/api/cat_requests'
+require 'megam/api/predef_clouds'
+require 'megam/api/cloud_tool_settings'
+require 'megam/api/sshkeys'
+require 'megam/api/marketplaces'
+require 'megam/api/marketplace_addons'
+require 'megam/api/organizations'
+require 'megam/api/domains'
+require 'megam/api/csars'
+require 'megam/api/assemblies'
+require 'megam/api/assembly'
+require 'megam/api/components'
+require 'megam/api/event'
+require 'megam/api/availableunits'
+require 'megam/api/balances'
+require 'megam/api/billinghistories'
+require 'megam/api/billings'
+require 'megam/api/credithistories'
+require 'megam/api/discounts'
+require 'megam/api/subscriptions'
+require 'megam/api/promos'
 
-require "megam/core/server_api"
-require "megam/core/config"
-require "megam/core/stuff"
-require "megam/core/text"
-require "megam/core/log"
-require "megam/core/json_compat"
-require "megam/core/auth"
-require "megam/core/error"
-require "megam/core/account"
-require "megam/core/request"
-require "megam/core/request_collection"
-require "megam/core/predefcloud"
-require "megam/core/predefcloud_collection"
-require "megam/core/cloudtoolsetting"
-require "megam/core/cloudtoolsetting_collection"
-require "megam/core/sshkey"
-require "megam/core/sshkey_collection"
-require "megam/core/marketplace"
-require "megam/core/marketplace_collection"
-require "megam/core/marketplace_addon"
-require "megam/core/marketplace_addon_collection"
-require "megam/core/organizations"
-require "megam/core/organizations_collection"
-require "megam/core/domains"
-require "megam/core/assemblies"
-require "megam/core/assemblies_collection"
-require "megam/core/csar"
-require "megam/core/csar_collection"
-require "megam/core/konipai"
-require "megam/core/assembly"
-require "megam/core/assembly_collection"
-require "megam/core/components"
-require "megam/core/components_collection"
-require "megam/core/cat_requests"
-require "megam/core/cat_requests_collection"
-require "megam/core/event"
+require 'megam/core/server_api'
+require 'megam/core/config'
+require 'megam/core/stuff'
+require 'megam/core/text'
+require 'megam/core/log'
+require 'megam/core/json_compat'
+require 'megam/core/auth'
+require 'megam/core/error'
+require 'megam/core/account'
+require 'megam/core/request'
+require 'megam/core/request_collection'
+require 'megam/core/predefcloud'
+require 'megam/core/predefcloud_collection'
+require 'megam/core/cloudtoolsetting'
+require 'megam/core/cloudtoolsetting_collection'
+require 'megam/core/sshkey'
+require 'megam/core/sshkey_collection'
+require 'megam/core/marketplace'
+require 'megam/core/marketplace_collection'
+require 'megam/core/marketplace_addon'
+require 'megam/core/marketplace_addon_collection'
+require 'megam/core/organizations'
+require 'megam/core/organizations_collection'
+require 'megam/core/domains'
+require 'megam/core/assemblies'
+require 'megam/core/assemblies_collection'
+require 'megam/core/csar'
+require 'megam/core/csar_collection'
+require 'megam/core/konipai'
+require 'megam/core/assembly'
+require 'megam/core/assembly_collection'
+require 'megam/core/components'
+require 'megam/core/components_collection'
+require 'megam/core/cat_requests'
+require 'megam/core/cat_requests_collection'
+require 'megam/core/event'
 
-
-require "megam/core/availableunits_collection"
-require "megam/core/availableunits"
-require "megam/core/balances_collection"
-require "megam/core/balances"
-require "megam/core/billinghistories_collection"
-require "megam/core/billinghistories"
-require "megam/core/billings_collection"
-require "megam/core/billings"
-require "megam/core/credithistories_collection"
-require "megam/core/credithistories"
-require "megam/core/discounts_collection"
-require "megam/core/discounts"
-require "megam/core/subscriptions_collection"
-require "megam/core/subscriptions"
-require "megam/core/promos"
+require 'megam/core/availableunits_collection'
+require 'megam/core/availableunits'
+require 'megam/core/balances_collection'
+require 'megam/core/balances'
+require 'megam/core/billinghistories_collection'
+require 'megam/core/billinghistories'
+require 'megam/core/billings_collection'
+require 'megam/core/billings'
+require 'megam/core/credithistories_collection'
+require 'megam/core/credithistories'
+require 'megam/core/discounts_collection'
+require 'megam/core/discounts'
+require 'megam/core/subscriptions_collection'
+require 'megam/core/subscriptions'
+require 'megam/core/promos'
 
 module Megam
   class API
-
-    #text is used to print stuff in the terminal (message, log, info, warn etc.)
+    # text is used to print stuff in the terminal (message, log, info, warn etc.)
     attr_accessor :text
 
-    API_MEGAM_IO = "api.megam.io".freeze
-    API_VERSION2 = "/v2".freeze
+    API_MEGAM_IO = 'api.megam.io'.freeze
+    API_VERSION2 = '/v2'.freeze
 
-    X_Megam_DATE = "X-Megam-DATE".freeze
-    X_Megam_HMAC = "X-Megam-HMAC".freeze
-    X_Megam_OTTAI = "X-Megam-OTTAI".freeze
+    X_Megam_DATE = 'X-Megam-DATE'.freeze
+    X_Megam_HMAC = 'X-Megam-HMAC'.freeze
+    X_Megam_OTTAI = 'X-Megam-OTTAI'.freeze
 
     HEADERS = {
       'Accept' => 'application/json',
@@ -115,20 +110,17 @@ module Megam
     }
 
     OPTIONS = {
-      :headers => {},
-      :host => '127.0.0.1',
-      :nonblock => false,
-      :scheme => 'http'
+      headers: {},
+      host: '127.0.0.1',
+      nonblock: false,
+      scheme: 'http'
     }
-
 
     def text
       @text ||= Megam::Text.new(STDOUT, STDERR, STDIN, {})
     end
 
-    def last_response
-      @last_response
-    end
+    attr_reader :last_response
 
     # It is assumed that every API call will use an API_KEY/email. This ensures validity of the person
     # really the same guy on who he claims.
@@ -137,17 +129,17 @@ module Megam
     # 2. The options as passed via the instantiation of API will override global options. The ones that are passed are :email and :api_key and will
     # be  merged into a class variable @options
     # 3. Upon merge of the options, the api_key, email as available in the @options is deleted.
-    def initialize(options={})
+    def initialize(options = {})
       @options = OPTIONS.merge(options)
       @api_key = @options.delete(:api_key) || ENV['MEGAM_API_KEY']
       @email = @options.delete(:email)
-      raise ArgumentError, "You must specify [:email, :api_key]" if @email.nil? || @api_key.nil?
+      fail ArgumentError, 'You must specify [:email, :api_key]' if @email.nil? || @api_key.nil?
     end
 
-    def request(params,&block)
+    def request(params, &block)
       just_color_debug("#{@options[:path]}")
       start = Time.now
-      Megam::Log.debug("START")
+      Megam::Log.debug('START')
       params.each do |pkey, pvalue|
         Megam::Log.debug("> #{pkey}: #{pvalue}")
       end
@@ -170,35 +162,35 @@ module Megam
         reerror.set_backtrace(error.backtrace)
         Megam::Log.debug("#{reerror.response.body}")
         reerror.response.body = Megam::JSONCompat.from_json(reerror.response.body.chomp)
-        Megam::Log.debug("RESPONSE ERR: Ruby Object")
+        Megam::Log.debug('RESPONSE ERR: Ruby Object')
         Megam::Log.debug("#{reerror.response.body}")
         raise(reerror)
       end
 
       @last_response = response
-      Megam::Log.debug("RESPONSE: HTTP Status and Header Data")
+      Megam::Log.debug('RESPONSE: HTTP Status and Header Data')
       Megam::Log.debug("> HTTP #{response.remote_ip} #{response.status}")
 
       response.headers.each do |header, value|
         Megam::Log.debug("> #{header}: #{value}")
       end
-      Megam::Log.debug("End HTTP Status/Header Data.")
+      Megam::Log.debug('End HTTP Status/Header Data.')
 
       if response.body && !response.body.empty?
         if response.headers['Content-Encoding'] == 'gzip'
-          Megam::Log.debug("RESPONSE: Content-Encoding is gzip")
+          Megam::Log.debug('RESPONSE: Content-Encoding is gzip')
           response.body = Zlib::GzipReader.new(StringIO.new(response.body)).read
         end
-        Megam::Log.debug("RESPONSE: HTTP Body(JSON)")
+        Megam::Log.debug('RESPONSE: HTTP Body(JSON)')
         Megam::Log.debug("#{response.body}")
 
         begin
           unless response.headers[X_Megam_OTTAI]
             response.body = Megam::JSONCompat.from_json(response.body.chomp)
-            Megam::Log.debug("RESPONSE: Ruby Object")
+            Megam::Log.debug('RESPONSE: Ruby Object')
           else
             response.body = Megam::KoniPai.new.koni(response.body.chomp)
-            Megam::Log.debug("RESPONSE: KoniPai Object ")
+            Megam::Log.debug('RESPONSE: KoniPai Object ')
           end
           Megam::Log.debug("#{response.body}")
         rescue Exception => jsonerr
@@ -206,7 +198,7 @@ module Megam
           raise(jsonerr)
         end
       end
-      Megam::Log.debug("END(#{(Time.now - start).to_s}s)")
+      Megam::Log.debug("END(#{(Time.now - start)}s)")
       # reset (non-persistent) connection
       @connection.reset
       response
@@ -215,31 +207,27 @@ module Megam
     private
 
     def just_color_debug(path)
-      text.msg "--> #{text.color("(#{path})", :cyan,:bold)}"                  # Why " inside "
+      text.msg "--> #{text.color("(#{path})", :cyan, :bold)}"                  # Why " inside "
     end
 
-
-    #Make a lazy connection.
+    # Make a lazy connection.
     def connection
-      @options[:path] =API_VERSION2 + @options[:path]
+      @options[:path] = API_VERSION2 + @options[:path]
       encoded_api_header = encode_header(@options)
-      @options[:headers] = HEADERS.merge({
-        X_Megam_HMAC => encoded_api_header[:hmac],
-        X_Megam_DATE => encoded_api_header[:date],
-      }).merge(@options[:headers])
-  
+      @options[:headers] = HEADERS.merge(X_Megam_HMAC => encoded_api_header[:hmac],
+                                         X_Megam_DATE => encoded_api_header[:date]).merge(@options[:headers])
 
-      Megam::Log.debug("HTTP Request Data:")
+      Megam::Log.debug('HTTP Request Data:')
       Megam::Log.debug("> HTTP #{@options[:scheme]}://#{@options[:host]}")
       @options.each do |key, value|
         Megam::Log.debug("> #{key}: #{value}")
       end
-      Megam::Log.debug("End HTTP Request Data.")
-      if @options[:scheme] == "https"
-      @connection = Excon.new("#{@options[:scheme]}://#{@options[:host]}",@options)
+      Megam::Log.debug('End HTTP Request Data.')
+      if @options[:scheme] == 'https'
+        @connection = Excon.new("#{@options[:scheme]}://#{@options[:host]}", @options)
       else
-           Excon.defaults[:ssl_verify_peer] = false
-           @connection = Excon.new("#{@options[:scheme]}://#{@options[:host]}:9000",@options)
+        Excon.defaults[:ssl_verify_peer] = false
+        @connection = Excon.new("#{@options[:scheme]}://#{@options[:host]}:9000", @options)
       end
       @connection
     end
@@ -252,21 +240,19 @@ module Megam
     # :date
     # (Refer https://github.com/indykish/megamplay.git/test/AuthenticateSpec.scala)
     def encode_header(cmd_parms)
-      header_params ={}
+      header_params = {}
       body_digest = OpenSSL::Digest::MD5.digest(cmd_parms[:body])
       body_base64 = Base64.encode64(body_digest)
 
-      current_date = Time.now.strftime("%Y-%m-%d %H:%M")
+      current_date = Time.now.strftime('%Y-%m-%d %H:%M')
 
-      data="#{current_date}"+"\n"+"#{cmd_parms[:path]}"+"\n"+"#{body_base64}"
+      data = "#{current_date}" + "\n" + "#{cmd_parms[:path]}" + "\n" + "#{body_base64}"
 
       digest  = OpenSSL::Digest.new('sha1')
       movingFactor = data.rstrip!
       hash = OpenSSL::HMAC.hexdigest(digest, @api_key, movingFactor)
-      final_hmac = @email+':' + hash
-      header_params = { :hmac => final_hmac, :date => current_date}
+      final_hmac = @email + ':' + hash
+      header_params = { hmac: final_hmac, date: current_date }
     end
-
  end
-
 end
