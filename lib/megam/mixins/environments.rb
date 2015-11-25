959c47ddcb7272d54bee80c5dd3a109d873f3027
vijaykanthm28 <mvijaykanth@megam.io>
2015-11-25 18:46:27 +0530

    envs parse json

--- lib/megam/mixins/environments.rb
+++ lib/megam/mixins/environments.rb
@@ -4,20 +4,25 @@ module Megam
   class Environments
     include Nilavu::MegamAttributes
 
-      attr_reader :envs
-
+      attr_reader  :envs
     ATTRIBUTES = [
     ]
     def attributes
       ATTRIBUTES
     end
-
+    
     def initialize(params)
-      set_attributes(params)
+     temp = {}
+      params.each do |k,v|
+       temp[k[:key]]=k[:value]
+       ATTRIBUTES << k[:key]
+      end
+      set_attributes(temp)
     end
 
-    def to_array
-      array = []
+    def to_array 
+    a= [to_hash]
+    a
     end
   end
 end
