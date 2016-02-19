e736070108368eb19a04a91363156157372358d1
thomasalrin <thomasalrin@megam.io>
2016-02-19 14:50:22 +0530

    org_id in header

--- lib/megam/core/account.rb
+++ lib/megam/core/account.rb
@@ -15,7 +15,7 @@
 #
 module Megam
   class Account < Megam::ServerAPI
-    def initialize(email=nil, api_key=nil, host=nil, password=nil)
+    def initialize(o)
       @id = nil
       @email = nil
       @api_key = nil
@@ -28,7 +28,7 @@ module Megam
       @password_reset_sent_at = nil
       @created_at = nil
       @some_msg = {}
-      super(email, api_key, host, password)
+      super({:email => o[:email], :api_key => o[:api_key], :host => o[:host], :password => o[:password], :org_id => o[:org_id]})
     end
 
     #used by resque workers and any other background job
@@ -201,7 +201,7 @@ module Megam
     end
 
     def self.from_hash(o)
-      acct = self.new(o[:email], o[:api_key], o[:host])
+      acct = self.new({:email => o[:email], :api_key => o[:api_key], :host => o[:host], :password => o[:password], :org_id => o[:org_id]})
       acct.from_hash(o)
       acct
     end
@@ -241,7 +241,7 @@ module Megam
 
     # Load a account by email_p
     def self.show(o)
-      acct = self.new(o[:email], o[:api_key], o[:host], o[:password])
+      acct = self.new({:email => o[:email], :api_key => o[:api_key], :host => o[:host], :password => o[:password], :org_id => o[:org_id]})
       acct.megam_rest.get_accounts(o[:email])
     end
 
