9020b087db03dc751333ddb8fada7ff1f664d6fd
ranjithamca <ranjithar@megam.io>
2016-03-01 10:17:27 +0530

    change travis

--- lib/megam/core/marketplace.rb
+++ lib/megam/core/marketplace.rb
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+require 'rubygems'
 require 'hashie'
 
 module Megam
@@ -74,7 +75,7 @@ module Megam
         @flavor
       end
     end
-    
+
     def catorder(arg = nil)
       if !arg.nil?
         @catorder = arg
