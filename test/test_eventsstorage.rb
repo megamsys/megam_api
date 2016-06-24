c27d1a69bd4e68fe504aa1a8954576dc38141d17
rajesh-rajagopal <rajeshr@megam.io>
2016-06-24 15:59:10 +0530

    space removed.

--- test/test_eventsstorage.rb
+++ test/test_eventsstorage.rb
@@ -1,10 +1,6 @@
 require File.expand_path("#{File.dirname(__FILE__)}/test_helper")
 
 class TestApps < MiniTest::Unit::TestCase
-
-
-
-
 #=begin
 def test_list_eventsstorage
   response = megams.list_eventsstorage("0")
