4ea36f1de7b68b024e1533f5bc680aa32e18fa48
rathishvbr <rathishvbr@outlook.com>
2016-12-02 16:04:14 +0530

    Report collection update.

--- lib/megam/api/reports.rb
+++ lib/megam/api/reports.rb
@@ -1,7 +1,6 @@
 module Megam
   class API
     def post_reports(new_sps)
-     puts "56156156156156156156156156156156151561561561565+++++++++++++++++++++++++"
       @options = {:path => '/reports/content',
         :body => Megam::JSONCompat.to_json(new_sps)}.merge(@options)
 
